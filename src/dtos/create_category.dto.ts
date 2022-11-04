16d4ab38c79d997b774afd12974c954e7e797782
CarlosSu02 <carlossuplei@gmail.com>
2022-11-03 22:05:40 -0600

    fixed display of errors in validationCategory and renamed CreateCategory to CreateCategoryDto

--- src/dtos/create_category.dto.ts
+++ src/dtos/create_category.dto.ts
@@ -1,7 +1,7 @@
 
 import { Length, IsNotEmpty, IsOptional, IsString } from "class-validator";
 
-export class CreateCategory {
+export class CreateCategoryDto {
 
     @Length(3, 50)
     @IsNotEmpty()
--- src/services/categories.service.ts
+++ src/services/categories.service.ts
@@ -1,7 +1,8 @@
 
-import { CreateCategory } from "../dtos/create_category.dto";
+import { CreateCategoryDto } from "../dtos/create_category.dto";
 import { Category } from "../models/category.model";
 import { validate } from "class-validator";
+import { plainToClass } from 'class-transformer';
 
 interface ICategory {
     name: string,
@@ -23,7 +24,7 @@ class CategoriesService {
         /* const searchAllCategories = await Category.findAndCountAll({ attributes: ['id', 'name', 'description'], 
                                                              order: [['id', 'ASC']] }); */
 
-        const searchCategories = await Category.findAndCountAll({ limit: size, offset: page * size });                                                             
+        const searchCategories = await Category.findAndCountAll({ limit: size, offset: page * size, order: [['id', 'ASC']] });                                                             
 
         if (searchCategories.count === 0) throw new Error('There are no categories added!');
         
@@ -46,26 +47,30 @@ class CategoriesService {
 
     };
 
-    public validationCategory = async (object: any): Promise<ICategory> => {
+    public validationCategory = async (object: any): Promise<CreateCategoryDto> => {
 
-        const createCategory = new CreateCategory();
-        const { name, description } = object;
+        // const createCategoryDto = new CreateCategory();
+        // const { name, description } = object;
        
-        createCategory.name = name;
-        createCategory.description = description;
+        const validatedCategory = plainToClass(CreateCategoryDto, object)
 
         // const validated = await validate(createCategory).then(errors => errors);
 
         // console.log(validated);
 
-        const validated = await validate(createCategory).then(errors => {
+        const errors = await validate(validatedCategory).then(errors => {
             
             if (errors.length > 0) {
 
-                let constraints: object | undefined;
+                let constraints: any = [];
 
                 errors.forEach(err => {
-                    constraints = err.constraints;
+                    constraints.push({ 
+                        'Property ': err.property, 
+                        'Errors ': err.constraints 
+                    });
+                    // Object.assign(constraints, err.constraints)
+                    // constraints = err.constraints;
                 });
 
                 return constraints;
@@ -78,7 +83,7 @@ class CategoriesService {
 
         // console.log(Object.keys(validated!).length);
 
-        if (typeof validated !== 'undefined') throw new Error(JSON.stringify(validated)); 
+        if (typeof errors !== 'undefined') throw new Error(JSON.stringify(errors)); 
         
         // const info = JSON.stringify(validated);
 
@@ -86,7 +91,7 @@ class CategoriesService {
 
         // console.log(JSON.parse(info!));
 
-        return { name, description };
+        return validatedCategory;
 
         // return (typeof validated !== 'undefined')
         //     ? JSON.stringify(validated)
